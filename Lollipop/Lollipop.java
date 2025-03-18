4196e490dd38dad42d738436b425292ccf32273d
theMuffinnMan <164093037+theMuffinnMan@users.noreply.github.com>
2025-03-18 21:41:18 +1300

    Improved Code with constants

---
+++ Lollipop/Lollipop.java
@@ -0,0 +1,43 @@
+/*
+ * code for lollipop GUI
+ */
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.awt.Color;
+
+
+/**
+ * Write a description of class Lollipop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Lollipop
+{
+    // instance variables - replace the example below with your own
+    public static final double X = 300.0;
+    public static final double Y = 180.0;
+    public static final double SIZE = 80.0;
+    public static final double STICK = 200.0;
+    
+    public Lollipop()
+    {
+        UI.initialise();
+        UI.addButton("Quit", UI::quit);
+    }
+
+    /** draw a red lollipop on a stick */
+    private void drawLollipop(){
+        UI.setLineWidth(SIZE/8.0);// set line width to 10
+        UI.drawLine(X, Y, X, Y+STICK);// draw line (300, 200) to 300,400)
+        UI.setLineWidth(1);//set line width to 1
+        UI.setColor(Color.blue);// set color to blue
+        UI.fillOval(X-SIZE/2.0, Y-SIZE/2.0, SIZE, SIZE);//fill oval @(260, 160), 80x80
+    }
+    public static void main(String[] args){
+        Lollipop obj = new Lollipop();
+        obj.drawLollipop();
+    }
+}
