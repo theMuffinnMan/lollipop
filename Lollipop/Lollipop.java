0cba598dc69d652d159c4238569b9d498d2a2462
theMuffinnMan <164093037+theMuffinnMan@users.noreply.github.com>
2025-03-21 12:50:49 +1300

    Lollipop - fixed

--- Lollipop/Lollipop.java
+++ Lollipop/Lollipop.java
@@ -46,6 +46,6 @@ public class Lollipop
     }
     public static void main(String[] args){
         Lollipop obj = new Lollipop();
-        obj.drawLollipop();
+        obj.doDrawLollipop();
     }
 }
